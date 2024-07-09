81f8c1b9fe5b164889a4a6a71b4baae2794bc669
Sasza Węsierski <alewesij@onet.pl>
2024-07-09 19:06:21 +0200

    Optimize via Gauss elimination

--- determinant.py
+++ determinant.py
@@ -13,3 +13,21 @@ def Laplace_determinant(B):
         cofactor = ((-1) ** i) * A[0, i] * Laplace_determinant(minor)
         determinant += cofactor
     return determinant
+
+# More optimized code using Gauss elimination algorithm
+def determinant(B):
+    A = B.copy()
+    if A.shape[0] != A.shape[1]:
+        return f'{A} is non-square matrix'
+    n = A.shape[0]
+    tolerance = 1e-06
+    for i in range(n):
+        highest_column_coef = i + np.argmax(abs(A[i:n+1, i]))
+        if abs(A[highest_column_coef, i]) < tolerance:
+            continue
+        A[[i, highest_column_coef]] = A[[highest_column_coef, i]]
+
+        for j in range(i+1, n):
+            A[j, :] = A[j, :] - (A[j, i]/A[i, i])*A[i, :]
+    return np.prod(np.diag(A))
+
