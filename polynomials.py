71d288f9edb3857a2785bf47b58ca1ab98abf0f5
Sasza Węsierski <alewesij@onet.pl>
2024-07-12 16:31:04 +0200

    Optimize polynomial multiplication via FFT

--- polynomials.py
+++ polynomials.py
@@ -1,4 +1,7 @@
+from fft import recursive_FFT, inverse_recursive_FFT
+
 # Straightforward implementation
+
 
 def multiply_polynomials(x, y):
     n, m = len(x), len(y)
@@ -8,3 +11,10 @@ def multiply_polynomials(x, y):
             result[i+j] += x[i]*y[j]
     return result
 
+
+def FFT_multiply_polynomials(x, y):
+    n = len(x)
+    a = x+[0]*n
+    b = y+[0]*n
+    A = recursive_FFT(a)*recursive_FFT(b)
+    return inverse_recursive_FFT(A)/(2*n)
